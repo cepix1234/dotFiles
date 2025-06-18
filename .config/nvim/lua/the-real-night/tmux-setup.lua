d0e582c9eab4ab6aa2dd4d1e4e2998edd1f8a656
cepix1234 <bocepek027@gmail.com>
2025-06-18 23:22:31 +0200

    Add init lazy git to nvim start

--- .config/nvim/lua/the-real-night/tmux-setup.lua
+++ .config/nvim/lua/the-real-night/tmux-setup.lua
@@ -17,30 +17,38 @@ end
 
 local sessionId = mysplit(sessionStatus, ",")[3]
 local termianlWindowId = string.format("%s:%s.%s", sessionId, 2, 1)
+local lazygitWindowId = string.format("%s:%s.%s", sessionId, 3, 1)
+local lazyGitInit = false
 
 local tmuxWindows = Job:new({ 'tmux', 'list-windows', cwd = cwd }):sync()
 
-vim.notify('resut:' .. #tmuxWindows)
-if #tmuxWindows == 1 then
-    Job:new({ 'tmux', 'rename-window', '-t', '1', 'main:NVIM', cwd = cwd }):sync()
-    Job:new({ 'tmux', 'new-window', '-n', 'terminal', cwd = cwd }):sync()
-    Job:new({ 'tmux', 'select-window', '-t', '1', cwd = cwd }):sync()
-end
-
-local sendKeys = function(command)
-    Job:new({ 'tmux', 'send-keys', '-t', termianlWindowId, command, 'Enter', cwd = cwd }):sync()
+local sendKeys = function(command, windowId)
+    Job:new({ 'tmux', 'send-keys', '-t', windowId, command, 'Enter', cwd = cwd }):sync()
 end
 
 local clear_terminal = function()
     local command = string.format("%s", terminalClearCommand)
-    sendKeys(command)
+    sendKeys(command, termianlWindowId)
 end
 
 local dir_change_function = function(dir)
     vim.notify('changing to:' .. dir)
     local command = string.format("%s %s", dirChangeCommand, dir)
-    sendKeys(command)
+    sendKeys(command, termianlWindowId)
     clear_terminal()
+    if lazyGitInit == false then
+        sendKeys(command, lazygitWindowId)
+        sendKeys("lazygit", lazygitWindowId)
+        lazyGitInit = true
+    end
+end
+
+vim.notify('resut:' .. #tmuxWindows)
+if #tmuxWindows == 1 then
+    Job:new({ 'tmux', 'rename-window', '-t', '1', 'main:NVIM', cwd = cwd }):sync()
+    Job:new({ 'tmux', 'new-window', '-n', 'terminal', cwd = cwd }):sync()
+    Job:new({ 'tmux', 'new-window', '-n', 'lazygit', cwd = cwd }):sync()
+    Job:new({ 'tmux', 'select-window', '-t', '1', cwd = cwd }):sync()
 end
 
 return {
