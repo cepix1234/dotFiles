84e47dbeab066d290d03489aa95111fba01ea7bc
blaz Ocepek <bocepek027@gmail.com>
2025-06-19 13:11:54 +0200

    Update markdown-preview.lua

--- .config/nvim/lua/the-real-night/lazy/misc/markdown-preview.lua
+++ .config/nvim/lua/the-real-night/lazy/misc/markdown-preview.lua
@@ -2,16 +2,12 @@ return
 {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-    ft = { "markdown" },
-    build = function(plugin)
-      if vim.fn.executable "npx" then
-        vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
-      else
-        vim.cmd [[Lazy load markdown-preview.nvim]]
-        vim.fn["mkdp#util#install"]()
-      end
+    init = function()
+        vim.g.mkdp_filetypes = { "markdown" }
     end,
+    ft = { "markdown" },
     config = function()
+        vim.fn["mkdp#util#install"]()
         vim.keymap.set("n", "<leader>mp", '::MarkdownPreviewToggle<cr>', { silent = false })
     end,
 }
