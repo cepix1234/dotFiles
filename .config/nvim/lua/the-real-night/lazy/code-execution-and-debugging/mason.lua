3653c69424b8566433fc7768bc58bbfaa42ae6d6
cepix1234 <bocepek027@gmail.com>
2025-08-27 21:30:30 +0200

    Fix mason install default packges

--- .config/nvim/lua/the-real-night/lazy/code-execution-and-debugging/mason.lua
+++ .config/nvim/lua/the-real-night/lazy/code-execution-and-debugging/mason.lua
@@ -15,7 +15,6 @@ return {
             require("mason").setup()
 
             local pkgs_candidates = {}
-            table.insert(pkgs_candidates, "js-debug-adapter")
             table.insert(pkgs_candidates, "csharp-language-server")
             table.insert(pkgs_candidates, "eslint-lsp")
             table.insert(pkgs_candidates, "gopls")
@@ -29,17 +28,23 @@ return {
             table.insert(pkgs_candidates, "typescript-language-server")
             table.insert(pkgs_candidates, "netcoredbg")
             table.insert(pkgs_candidates, "go-debug-adapter")
+            table.insert(pkgs_candidates, "cpptools")
 
             -- only install what is not already installed
             local Regestry = require("mason-registry")
             local instaled_pkgs = Regestry.get_installed_package_names()
             local pkgs_to_install = {}
-            for i, v in ipairs(instaled_pkgs) do
-                if not has_value(pkgs_candidates, v) then
-                    table.insert(v)
+            for i, v in ipairs(pkgs_candidates) do
+                if not has_value(instaled_pkgs, v) then
+                    table.insert(pkgs_to_install, v)
                 end
             end
 
+            text = ""
+            for i, v in ipairs(pkgs_to_install) do
+                text = text .. ", " .. v
+            end
+            vim.notify('MASON TO INTALL:' .. text)
             local Command = require "mason.api.command"
             if not vim.tbl_isempty(pkgs_to_install) then
                 Command.MasonInstall(pkgs_to_install)
