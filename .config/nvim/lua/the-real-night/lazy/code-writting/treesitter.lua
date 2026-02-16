61ac0c28ad02b490173d4e27a3760cfec87fbfe3
blaz ocepek <blaz.ocepek@adacta-fintech.com>
2026-02-16 09:31:07 +0100

    Fix treesitter and telescope

--- .config/nvim/lua/the-real-night/lazy/code-writting/treesitter.lua
+++ .config/nvim/lua/the-real-night/lazy/code-writting/treesitter.lua
@@ -2,7 +2,7 @@ return {
     "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     config = function()
-        require("nvim-treesitter.configs").setup({
+        require("nvim-treesitter").setup({
             -- A list of parser names, or "all"
             ensure_installed = {
                 "vimdoc", "javascript", "typescript", "go", "c_sharp", "c", "lua", "rust",
@@ -32,7 +32,7 @@ return {
             },
         })
 
-        local treesitter_parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+        local treesitter_parser_config = require("nvim-treesitter.parsers")
         treesitter_parser_config.templ = {
             install_info = {
                 url = "https://github.com/vrischmann/tree-sitter-templ.git",
--- .config/nvim/lua/the-real-night/lazy/navigation/telescope.lua
+++ .config/nvim/lua/the-real-night/lazy/navigation/telescope.lua
@@ -1,9 +1,6 @@
 return {
     checker = { enabled = true },
     "nvim-telescope/telescope.nvim",
-
-    tag = "0.1.5",
-
     dependencies = {
         "nvim-lua/plenary.nvim"
     },
