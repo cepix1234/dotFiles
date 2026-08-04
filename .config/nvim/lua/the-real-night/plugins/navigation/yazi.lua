69ceeb3faa9c2d83063972edcd6f2fffa828981a
blaz ocepek <blaz.ocepek@adacta-fintech.com>
2026-08-04 08:55:31 +0200

    fix the yazi to ope and focus on start

--- .config/nvim/lua/the-real-night/plugins/navigation/yazi.lua
+++ .config/nvim/lua/the-real-night/plugins/navigation/yazi.lua
@@ -3,7 +3,11 @@ vim.pack.add({
     'https://github.com/mikavilpas/yazi.nvim'
 })
 
-require("yazi").setup({
-    -- Replace netrw when opening a directory (e.g. `nvim .`)
-    open_for_directories = true,
+vim.g.loaded_netrwPlugin = 1
+vim.api.nvim_create_autocmd("UIEnter", {
+    callback = function()
+        require("yazi").setup({
+            open_for_directories = true,
+        })
+    end,
 })
